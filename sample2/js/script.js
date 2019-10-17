a654673023db898f06e6c0bc0703ada30aae71d8
Tomoya Suemori <basuke13ninjin@gmail.com>
2019-10-17 16:39:06 +0900

    迷路ゲームのサンプル

--- sample2/js/script.js
+++ sample2/js/script.js
@@ -36,6 +36,10 @@ right.img.src = 'img/right.png';
 var mapchip = new Image();
 mapchip.src = 'img/wb.png';
 
+//ゴールスタートの赤表示
+var red = new Image();
+red.src = 'img/red.png';
+
 //キーボードのオブジェクトを作成
 var key = new Object();
 key.up = false;
@@ -46,7 +50,7 @@ key.push = '';
 
 //マップの作成（さくせい）
 var map = [
-  [0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+  [2, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
   [0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0],
   [0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
   [1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0],
@@ -65,7 +69,7 @@ var map = [
   [0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0],
   [0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1],
   [0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
-  [0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0]
+  [0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 2]
 ]
 
 //メインループ
@@ -110,6 +114,7 @@ function main() {
     for (var x = 0; x < map[y].length; x++) {
       if (map[y][x] === 0) ctx.drawImage(mapchip, 0, 0, 32, 32, 32 * x, 32 * y, 32, 32);
       if (map[y][x] === 1) ctx.drawImage(mapchip, 32, 0, 32, 32, 32 * x, 32 * y, 32, 32);
+      if (map[y][x] === 2) ctx.drawImage(red, 0, 0, 32, 32, 32 * x, 32 * y, 32, 32);
     }
   }
 
@@ -128,7 +133,7 @@ function main() {
       var x = tree.x / 32;
       var y = tree.y / 32;
       x--;
-      if (map[y][x] === 0) {
+      if (map[y][x] === 0 || map[y][x] === 2) {
         tree.move = 32;
         key.push = 'left';
 
@@ -139,7 +144,7 @@ function main() {
       var y = tree.y / 32;
       if (y > 0) {
         y--;
-        if (map[y][x] === 0) {
+        if (map[y][x] === 0 || map[y][x] === 2) {
           tree.move = 32;
           key.push = 'up';
 
@@ -150,7 +155,7 @@ function main() {
       var x = tree.x / 32;
       var y = tree.y / 32;
       x++;
-      if (map[y][x] === 0) {
+      if (map[y][x] === 0 || map[y][x] === 2) {
         tree.move = 32;
         key.push = 'right';
 
@@ -161,7 +166,7 @@ function main() {
       var y = tree.y / 32;
       if (y < 19) {
         y++;
-        if (map[y][x] === 0) {
+        if (map[y][x] === 0 || map[y][x] === 2) {
           tree.move = 32;
           key.push = 'down';
 
