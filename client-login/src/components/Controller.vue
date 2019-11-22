209516b7024b662c5ef96a750f7b8b792fe743a5
Tomoya Suemori <basuke13ninjin@gmail.com>
2019-11-22 17:23:24 +0900

    ログイン画面の作成

---
+++ client-login/src/components/Controller.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <table style="table-layout: fixed;">
+      <tr>
+        <td></td>
+        <td>
+          <b-button @click="onUpClick" :disabled="disabledUp"
+            ><font-awesome-icon icon="arrow-up" size="2x"
+          /></b-button>
+        </td>
+        <td>
+          <b-button @click="onBombClick" :disabled="disabledBomb"
+            ><font-awesome-icon icon="bomb" size="2x"
+          /></b-button>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <b-button @click="onLeftClick" :disabled="disabledLeft"
+            ><font-awesome-icon icon="arrow-left" size="2x"
+          /></b-button>
+        </td>
+        <td>
+          <b-button @click="onEndClick" :disabled="disabledEnd"
+            ><font-awesome-icon icon="check-square" size="2x"
+          /></b-button>
+        </td>
+        <td>
+          <b-button @click="onRightClick" :disabled="disabledRight"
+            ><font-awesome-icon icon="arrow-right" size="2x"
+          /></b-button>
+        </td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>
+          <b-button @click="onDownClick" :disabled="disabledDown"
+            ><font-awesome-icon icon="arrow-down" size="2x"
+          /></b-button>
+        </td>
+        <td>
+          <b-button @click="onRedoClick" :disabled="disabledRedo"
+            ><font-awesome-icon icon="redo" size="2x"
+          /></b-button>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { MOVE } from "../App.vue";
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Controller extends Vue {
+  @Prop() disabledUp!: boolean;
+  @Prop() disabledLeft!: boolean;
+  @Prop() disabledRight!: boolean;
+  @Prop() disabledDown!: boolean;
+  @Prop() disabledRedo!: boolean;
+  @Prop() disabledBomb!: boolean;
+  @Prop() disabledEnd!: boolean;
+  private onUpClick() {
+    this.$emit("click", MOVE.UP);
+  }
+
+  private onLeftClick() {
+    this.$emit("click", MOVE.LEFT);
+  }
+
+  private onRightClick() {
+    this.$emit("click", MOVE.RIGHT);
+  }
+
+  private onDownClick() {
+    this.$emit("click", MOVE.DOWN);
+  }
+
+  private onRedoClick() {
+    this.$emit("click", MOVE.REDO);
+  }
+
+  private onBombClick() {
+    this.$emit("click", MOVE.BOMB);
+  }
+
+  private onEndClick() {
+    this.$emit("click", MOVE.END);
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ client-login/src/components/State.vue
@@ -0,0 +1,99 @@
+<template>
+  <div style="margin-right: 20px;">
+    <div v-for="(m, index) in select" :key="index" class="frame_stroke_rect">
+      <font-awesome-icon
+        v-if="m === MOVE.UP"
+        icon="arrow-alt-circle-up"
+        size="4x"
+      />
+      <font-awesome-icon
+        v-else-if="m === MOVE.LEFT"
+        icon="arrow-alt-circle-left"
+        size="4x"
+      />
+      <font-awesome-icon
+        v-else-if="m === MOVE.RIGHT"
+        icon="arrow-alt-circle-right"
+        size="4x"
+      />
+      <font-awesome-icon
+        v-else-if="m === MOVE.DOWN"
+        icon="arrow-alt-circle-down"
+        size="4x"
+      />
+      <font-awesome-icon v-else-if="m === MOVE.BOMB" icon="bomb" size="4x" />
+    </div>
+    <div
+      id="player1button"
+      style="margin-top: 20px;"
+      class="btn-square"
+      :class="turn ? (player1 ? 'player1' : 'player2') : 'player'"
+    >
+      {{ player1 ? 1 : 2 }}P
+    </div>
+    <div style="margin-top: 20px;">
+      <font-awesome-icon
+        icon="bomb"
+        size="2x"
+        style="cursor: pointer; position: relative; float:left;"
+      />
+      <div style="float: left; margin-top: 8px;">x</div>
+      <div class="bomb">{{ bomb }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { MOVE } from "../App.vue";
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class State extends Vue {
+  @Prop({ default: true }) private player1!: boolean;
+  @Prop({ default: 5 }) private bomb!: number;
+  @Prop({ default: true }) private turn!: boolean;
+  @Prop() select!: string[];
+
+  get MOVE() {
+    return MOVE;
+  }
+}
+</script>
+
+<style scoped>
+.frame_stroke_rect {
+  border: solid 1px #000;
+  width: 64px;
+  height: 64px;
+  text-align: center;
+  vertical-align: middle;
+}
+
+.btn-square {
+  display: inline-block;
+  padding: 0.5em 1.5em;
+  text-decoration: none;
+  color: #fff;
+  border-radius: 3px;
+}
+
+.player1 {
+  background: #668ad8;
+  border-bottom: solid 4px #627295;
+}
+
+.player2 {
+  background: #d86695;
+  border-bottom: solid 4px #956286;
+}
+
+.player {
+  background: #d1c3c9;
+  border-bottom: solid 4px #979296;
+}
+
+.bomb {
+  color: red;
+  font-size: 28px;
+}
+</style>
